4fb7b76ea852c053adbb80b6119ad44d7388927f
Ricardo Kustner <ricardo.kustner@postnl.nl>
2021-02-25 13:06:52 +0100

    lamba js codeql test

--- cdk-sample/lambda/hello.js
+++ cdk-sample/lambda/hello.js
@@ -1,5 +1,13 @@
 exports.handler = async function(event) {
     console.log("request:", JSON.stringify(event, undefined, 2));
+    const codeqltest = codeqltest &&;
+
+    // let's see if codeql catches this
+    try {
+      url = JSON.parse(window.name).bootstrap; 
+    } catch (e) {}
+    document.write('<script src="' + url + '"><' + '/script>');
+
     return {
       statusCode: 200,
       headers: { "Content-Type": "text/plain" },
